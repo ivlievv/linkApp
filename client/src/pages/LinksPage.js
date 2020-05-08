e354032d1b02ec5fde4557286e4aa1980d7fc49d
Ihor Ivliev <igor852745@gmail.com>
2020-05-08 18:28:18 +0300

    create LinksList.js and add table links in app

--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -2,6 +2,7 @@ import React, {useCallback, useContext, useEffect, useState} from "react";
 import {useHttp} from "../hooks/http.hook";
 import {AuthContext} from "../context/AuthContext";
 import {Loader} from "../components/Loader";
+import {LinksList} from "../components/LinksList";
 
 
 export const LinksPage = () => {
