1909517c79fc4b95d7bb04567a766b348ea974ab
Ihor Ivliev <igor852745@gmail.com>
2020-05-08 20:06:57 +0300

    redirect commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json({extended: true}))
 
 app.use('/api/auth', require('./route/auth.routes'))
 app.use('/api/link',require('./route/link.routes'))
+app.use('/t',require('./route/redirect.routes'))
 
 const PORT = config.get('port') || 5000
 
